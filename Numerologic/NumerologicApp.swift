a0a1f170bc50108e688b57dc006d96ed90feb5b4
Nayyan Mujadiya <nayaneye@gmail.com>
2024-06-19 16:57:36 +0800

    Initial Commit

---
+++ Numerologic/NumerologicApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumerologicApp.swift
+//  Numerologic
+//
+//  Created by user261529 on 6/19/24.
+//
+
+import SwiftUI
+
+@main
+struct NumerologicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
